a1bd079a0dbd7f55cc35e675e4a4b7292a39a658
KamAlien <kamilareinoso25junio2005@gmail.com>
2026-05-02 06:49:34 -0400

    Mixto

---
+++ tests/test_hito3/Mixto.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    volatile long long i;
+    int ciclo = i;
+    printf("----Proceso Camaleon----\n");
+    printf("Mi pid es %d\n", getpid());
+    while (1) {
+        // Hacer algo intensivo en CPU
+        for(i = 0; i < 1000000000; i++) {
+    
+        }
+
+        for(int j = 0; j < 10; j++) {
+            // Hacer algo no intensivo en CPU
+            sleep(2); // Dormir un poco para simular un proceso no intensivo
+        }
+    }
+    return 0;
+}
