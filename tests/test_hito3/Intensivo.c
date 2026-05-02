9ee5660d735c35c27090a723b5f41c93b71c0407
KamAlien <kamilareinoso25junio2005@gmail.com>
2026-05-02 05:55:24 -0400

    arreglamdo

--- tests/test_hito3/Intensivo.c
+++ tests/test_hito3/Intensivo.c
@@ -3,7 +3,7 @@
 int main()
 {
     while (1) {
-        for(volatile int i = 0; i < 1000000; i++); // Hacer algo intensivo en CPU
+        for(volatile int i = 0; i < 1000000; i++) // Hacer algo intensivo en CPU
         {
             sleep(1); // Dormir un poco para simular un proceso no intensivo
         }
