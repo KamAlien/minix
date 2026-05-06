61fff27fca10c61f9780c9d61185d21acf39afb3
KamAlien <kamilareinoso25junio2005@gmail.com>
2026-05-06 12:41:34 -0400

    cambio

--- tests/test_hito3/Mixto.c
+++ tests/test_hito3/Mixto.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/resource.h>
+
+void print_sched_priority(const char *label)
+{
+    char cmd[64];
+    char buf[256];
+    FILE *f;
+    snprintf(cmd, sizeof(cmd), "ps -l | grep '%d'", getpid());
+    f = popen(cmd. "r");
+    if(f)
+    {
+        while (fgets(buf, sizeof(buf), f))
+        {
+            printf("[%s] %s", label, buf);
+
+        }
+        pelose(f);
+    }
+    fflush(stdout);
+}
 int main()
 {
     volatile long long i;
@@ -12,10 +32,12 @@ int main()
     
         }
 
-        for(int j = 0; j < 40; j++) {
+        for(int j = 0; j < 10; j++) {
             // Hacer algo no intensivo en CPU
             sleep(2); // Dormir un poco para simular un proceso no intensivo
         }
+        print_sched_priority("mixto")
+
     }
     return 0;
 }
